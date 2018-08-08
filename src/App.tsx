f918418b34398576ff6a2364f714fd3c86c85ac2
Ramon Dioneda <dionedarj@gmail.com>
2018-08-07 22:50:57 -0700

    Changed styles, made button do stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import IAppState from  './interfaces/App.interface';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Button from '@material-ui/core/Button';
 
 import * as React from 'react';
@@ -12,40 +13,57 @@ class App extends React.Component<{}, IAppState> {
       password: '',
       username: ''
     }
-    this.changeName = this.changeName.bind(this);
-    this.changePass = this.changePass.bind(this);
   }
 
-  public changeName(e : any) {
+  public changeName = (e : any) => {
     this.setState({
-      username: e.target.value
+      username: e.target.value  
     })
   }
 
-  public changePass(e: any) {
+  public changePass = (e: any) => {
     this.setState({
       password: e.target.value
     })
   }
 
+  public alertName = (e: any) => {
+    alert(this.state.username)
+  }
+
   public render() {
+
+    const style: any = {
+      paddingTop: '30px',
+      display: 'flex',
+      flex: 1,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    }
+
+
     return (
-      <div>
-        <Button color='primary' variant='contained'>
-          Hello {this.state.username}
-        </Button>
-        <input
-          type='text'
-          value={this.state.password}
-          onChange={this.changePass}
-        />
-        <input
-          type='text'
-          value={this.state.username}
-          onChange={this.changeName}
-        />
-      </div>
-      
+      <React.Fragment>
+        <CssBaseline />
+          <div style={style}>
+            <input
+              type='text'
+              value={this.state.password}
+              onChange={this.changePass}
+            />
+            <input
+              type='text'
+              value={this.state.username}
+              onChange={this.changeName}
+            />
+          </div>
+          <div style={style}>
+            <Button color='primary' variant='contained' onClick={this.alertName}>
+              Hello {this.state.username}
+            </Button>
+          </div>
+      </React.Fragment>
     )
   }
 }
