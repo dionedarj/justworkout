eafbdec382df0bb389aa8fa82618f1cca91b7683
Ramon Dioneda <dionedarj@gmail.com>
2018-09-04 23:05:22 -0700

    Added login check, added react-router and react-redux to package

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,47 @@ import IAppState from  './interfaces/App.interface';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 import * as React from 'react';
 
 class App extends React.Component<{}, IAppState> {
   constructor(props: any) {
-    super(props)
+    super(props);
     this.state = {
       password: '',
       username: ''
-    }
+    };
   }
 
   public changeName = (e : any) => {
     this.setState({
       username: e.target.value  
     })
-  }
+  };
 
-  public changePass = (e: any) => {
+  public changePass = (e : any) => {
     this.setState({
       password: e.target.value
     })
-  }
+  };
 
-  public alertName = (e: any) => {
-    alert(this.state.username)
-  }
+  public login = () => {
+    alert('You logged in!');
+  };
+
+  public checkLoginEnter = (e : any) => {
+    if (e.key === 'Enter') {
+        this.checkLogin();
+        e.preventDefault();
+    }
+  };
+
+  public checkLogin = () => {
+      if (this.state.password.localeCompare('password') === 0 && this.state.username.localeCompare('username') === 0) {
+          this.login();
+      }
+  };
 
   public render() {
 
@@ -40,31 +54,23 @@ class App extends React.Component<{}, IAppState> {
       flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
-    }
+    };
 
 
     return (
       <React.Fragment>
         <CssBaseline />
           <div style={style}>
-            <input
-              type='text'
-              value={this.state.password}
-              onChange={this.changePass}
-            />
-            <input
-              type='text'
-              value={this.state.username}
-              onChange={this.changeName}
-            />
+            <TextField onChange={this.changeName} onKeyPress={this.checkLoginEnter}/>
+            <TextField onChange={this.changePass} onKeyPress={this.checkLoginEnter}/>
           </div>
           <div style={style}>
-            <Button color='primary' variant='contained' onClick={this.alertName}>
+            <Button color='primary' variant='contained' onClick={this.checkLogin}>
               Hello {this.state.username}
             </Button>
           </div>
       </React.Fragment>
-    )
+    );
   }
 }
 
